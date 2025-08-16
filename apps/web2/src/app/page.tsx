1f6d1c571cef98a3da9325b127b7780876b2d04b
toyjack <toyjack@gmail.com>
2025-08-16 23:47:57 +0900

    feat(page.tsx): enhance Home component layout with heading and link

--- apps/web2/src/app/page.tsx
+++ apps/web2/src/app/page.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link"
+
 function Home() {
   return (
-    <div>Hello world! Again!</div>
+    <div>
+      <h2>Hello world! Again!</h2>
+      <Link href="/api">api</Link>
+    </div>
   )
 }
 export default Home 
